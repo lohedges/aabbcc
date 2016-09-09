463c615b5226219e7782d69af6e88383ad3bff14
Lester Hedges <lester.hedges@gmail.com>
2016-09-09 11:43:13 +0100

    Compile with support for the C++11 standard.

--- python/setup.py
+++ python/setup.py
@@ -7,15 +7,16 @@ setup.py file for the AABB.cc python interface.
 from distutils.core import setup, Extension
 
 aabb_module = Extension('_aabb',
-                         sources=['aabb_wrap.cxx', '../src/AABB.cc'],
+                         sources = ['aabb_wrap.cxx', '../src/AABB.cc'],
+                         extra_compile_args = ['-std=c++11'],
                         )
 
 setup (name = 'aabb',
        author = 'Lester Hedges',
-       author_email='lester.hedges+aabbcc@gmail.com',
-       description='AABB.cc python wrapper',
+       author_email = 'lester.hedges+aabbcc@gmail.com',
+       description = 'AABB.cc python wrapper',
        ext_modules = [aabb_module],
        py_modules = ['aabb'],
-       url='http://github.com/lohedges/aabbcc',
-       license='Zlib',
+       url = 'http://github.com/lohedges/aabbcc',
+       license = 'Zlib',
        )
