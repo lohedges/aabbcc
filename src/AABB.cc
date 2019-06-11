1b18ef76ef14cec79088975a61a09957fa85b4da
Lester Hedges <lester.hedges@gmail.com>
2019-06-11 16:02:46 +0100

    Use std::vector to avoid compiler warnings. [ci skip]

--- src/AABB.cc
+++ src/AABB.cc
@@ -403,7 +403,7 @@ namespace aabb
         unsigned int node = allocateNode();
 
         // AABB size in each dimension.
-        double size[dimension];
+        std::vector<double> size(dimension);
 
         // Compute the AABB limits.
         for (unsigned int i=0;i<dimension;i++)
@@ -549,7 +549,7 @@ namespace aabb
         assert(nodes[node].isLeaf());
 
         // AABB size in each dimension.
-        double size[dimension];
+        std::vector<double> size(dimension);
 
         // Compute the AABB limits.
         for (unsigned int i=0;i<dimension;i++)
